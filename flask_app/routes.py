d7e1b913f3bf4fa0046ba69be6d75e4065b28b3e
Smital Desai <desaismital@gmail.com>
2023-07-13 13:19:42 +0530

    Add support for working templates

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
+
 from flask_app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
+    mock_user = {'username': 'Smital'}
+    mock_posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title="smital", user=mock_user, posts=mock_posts)
